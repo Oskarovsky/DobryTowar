7a1e1caa582016cfefdc0a0628d9e3f599375beb
Oskar Slyk <oskar.slyk@ibm.com>
2019-08-15 19:40:22 +0200

    redirecting on product added

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    template: "./cartDetail.component.html"
+    selector: "cart-detail",
+    template: `<div><h3 class="bg-info p-1 text-white">Cart Detail Component</h3></div>`
 })
 export class CartDetailComponent { }
