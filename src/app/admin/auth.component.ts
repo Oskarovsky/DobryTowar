1d4bc053881bbb99c2cf8a2c393fb903e03e9483
Oskar Slyk <oskar.slyk@ibm.com>
2019-08-16 08:23:00 +0200

    admin authentication`

---
+++ src/app/admin/auth.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+
+@Component({
+    templateUrl: "auth.component.ts"
+})
+export class AuthComponent {
+    public username: String;
+    public password: String;
+    public errorMessage: String;
+
+    constructor(private router: Router) { }
+
+    authenticate(form: NgForm) {
+        if (form.valid) {
+            //performing authentication
+            this.router.navigateByUrl("/admin/main");
+        } else {
+            this.errorMessage = "Form data invalid";
+        }
+    }
+}
