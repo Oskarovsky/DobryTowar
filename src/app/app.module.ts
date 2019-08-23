241e66a482b62f1709b82b12a7544746be76bf10
Oskar Slyk <oskar.slyk@ibm.com>
2019-08-23 12:32:28 +0200

    auth fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
 import { StoreComponent } from "./store/store.component";
@@ -16,7 +15,6 @@ import { StoreFirstGuard } from "./storeFirst.guard";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     StoreModule,
     RouterModule.forRoot([
       { 
